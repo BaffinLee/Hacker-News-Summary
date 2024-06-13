6a13eea4794195212ed088f482f1e58e796b3a72
Baffin Lee <baffinlee@gmail.com>
2024-06-13 16:43:57 +0000

    fix: fix empty url error

--- backend/controller/scrape-news.ts
+++ backend/controller/scrape-news.ts
@@ -11,12 +11,12 @@ export async function scrapeNews(ctx?: OptionalContext) {
     const newsList: NewsInfo[] = [];
     for (let id of newsIdsToSave.slice(0, FETCH_DETAIL_SIZE)) {
         const news = await fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`).then(res => res.json());
-        if (news.type !== 'story') continue;
+        if (news.type !== 'story' || !news.id) continue;
         newsList.push({
             id: news.id,
-            user: news.by,
-            title: news.title,
-            url: news.url,
+            user: news.by || '',
+            title: news.title || '',
+            url: news.url || `https://news.ycombinator.com/item?id=${news.id}`,
         });
         console.log(`fetched news: ${news.title} by @${news.by}`);
     }
